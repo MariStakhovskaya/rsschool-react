ea9aef117f0057f846e6e5ddb561f81183bea746
Mari Stakhovskaya <maryia.jegorova@gmail.com>
2023-03-14 12:40:18 +0300

    refactor: add folders in project

--- components/src/app/App.tsx
+++ components/src/app/App.tsx
@@ -1,8 +1,21 @@
+import About from 'pages/about/About';
+import ErrorPage from 'pages/error/ErrorPage';
+import Main from 'pages/main/Main';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <div className="App">Start</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index element={<About />} />
+        </Route>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ components/src/pages/main/Main.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Main() {
+  return <div className="App">1.Search Bar. 2.Cards</div>;
+}
+
+export default Main;
