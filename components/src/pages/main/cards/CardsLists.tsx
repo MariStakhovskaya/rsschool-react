1e76bddedb7031e96fa4364daa40a9d86ec371bf
Mari Stakhovskaya <maryia.jegorova@gmail.com>
2023-04-10 01:08:45 +0300

    refactor: refactor modal

--- components/src/pages/main/cards/CardsLists.tsx
+++ components/src/pages/main/cards/CardsLists.tsx
@@ -6,7 +6,6 @@ import React, { useState } from 'react';
 
 import { cardsStateType } from '../Main';
 import Modal from 'widgets/modal/Modal';
-import icon from '../../../assets/icon.png';
 
 type propsType = {
   cards: Array<cardsStateType>;
@@ -25,60 +24,7 @@ function CardsLists({ cards }: propsType) {
           <Card key={card.id} cards={card} setActive={setActive} setModalCard={setModalCard} />
         ))
       )}
-      {active ? (
-        <Modal active={active} setActive={setActive}>
-          {modalCard && (
-            <div className={style.modalInfo}>
-              <div className={style.modalInfo__left}>
-                <div className={`${style.imgContainer} + ${style.exmpl}`}>
-                  <img src={modalCard.urls.regular} />
-                </div>
-                <div>
-                  <h5>{new Date(modalCard.created_at).getDay()}</h5>
-                  <h6>{new Date(modalCard.created_at).toLocaleString('en', { month: 'long' })}</h6>
-                </div>
-                <div className={style.iconLike}>
-                  <img src={icon} />
-                  <span>{modalCard.likes}</span>
-                </div>
-              </div>
-              <div className={style.modalInfo__right}>
-                <div className={style.closeModal} onClick={() => setActive(false)}>
-                  x
-                </div>
-                <div className={style.userInfo}>
-                  <div>
-                    <img src={modalCard.user.profile_image.medium} />
-                  </div>
-                  <div className={style.userInfoName}>
-                    <div>
-                      <b>Autor: </b> {modalCard.user.name}
-                    </div>
-                    <div>
-                      <b>Location: </b> {modalCard.location.name}
-                    </div>
-                  </div>
-                </div>
-                <div className={style.separator}></div>
-                <div>
-                  <b>Description: </b>
-                  <div>
-                    {modalCard.description === null ? 'no descriprion' : modalCard.description}
-                  </div>
-                </div>
-                <div className={style.tags}>
-                  <b>Tags: </b>
-                  {modalCard.tags.map((tag, index) => (
-                    <div key={index}>{' ' + '#' + tag.title + ' '}</div>
-                  ))}
-                </div>
-              </div>
-            </div>
-          )}
-        </Modal>
-      ) : (
-        ''
-      )}
+      {active ? <Modal active={active} setActive={setActive} modalCard={modalCard} /> : ''}
     </div>
   );
 }
--- components/src/widgets/modal/Modal.tsx
+++ components/src/widgets/modal/Modal.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import style from './Modal.module.css';
+import { cardModalType } from 'pages/main/cards/Card/Card';
+import icon from '../../assets/icon.png';
 
 type modalPropsType = {
   active: boolean;
   setActive: (active: boolean) => void;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  children: any;
+  modalCard: cardModalType | undefined;
 };
 
-const Modal = ({ active, setActive, children }: modalPropsType) => {
+const Modal = ({ active, setActive, modalCard }: modalPropsType) => {
   return (
     <div
       className={active ? `${style.modal + ' ' + style.active}` : style.modal}
@@ -18,7 +19,54 @@ const Modal = ({ active, setActive, children }: modalPropsType) => {
         className={active ? `${style.modalContent} ${style.active}` : style.modalContent}
         onClick={(e) => e.stopPropagation()}
       >
-        {children}
+        {modalCard && (
+          <div className={style.modalInfo}>
+            <div className={style.modalInfo__left}>
+              <div className={`${style.imgContainer} + ${style.exmpl}`}>
+                <img src={modalCard.urls.regular} />
+              </div>
+              <div>
+                <h5>{new Date(modalCard.created_at).toLocaleString('en', { day: 'numeric' })}</h5>
+                <h6>{new Date(modalCard.created_at).toLocaleString('en', { month: 'long' })}</h6>
+              </div>
+              <div className={style.iconLike}>
+                <img src={icon} />
+                <span>{modalCard.likes}</span>
+              </div>
+            </div>
+            <div className={style.modalInfo__right}>
+              <div className={style.closeModal} onClick={() => setActive(false)}>
+                x
+              </div>
+              <div className={style.userInfo}>
+                <div>
+                  <img src={modalCard.user.profile_image.medium} />
+                </div>
+                <div className={style.userInfoName}>
+                  <div>
+                    <b>Autor: </b> {modalCard.user.name}
+                  </div>
+                  <div>
+                    <b>Location: </b> {modalCard.location.name}
+                  </div>
+                </div>
+              </div>
+              <div className={style.separator}></div>
+              <div>
+                <b>Description: </b>
+                <div>
+                  {modalCard.description === null ? 'no descriprion' : modalCard.description}
+                </div>
+              </div>
+              <div className={style.tags}>
+                <b>Tags: </b>
+                {modalCard.tags.map((tag, index) => (
+                  <div key={index}>{' ' + '#' + tag.title + ' '}</div>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
